fe7a2f3d4cd15c063c1012a66405496a5c1c980b
Shichisan <taiga.shichisan@gmail.com>
2020-12-13 00:21:09 +0900

    :+1: apply composite pattern

--- app/models/detail.rb
+++ app/models/detail.rb
@@ -1,4 +1,6 @@
 class Detail < ApplicationRecord
+  include ParticularComponent
+
   validates :amount, presence: true
   validates :category_id, presence: true
   validates :user_id, presence: true
@@ -6,4 +8,15 @@ class Detail < ApplicationRecord
 
   belongs_to :user
   belongs_to :category
+  belongs_to :group
+
+  def add_to_group(group)
+    self.group = group
+    save
+  end
+
+  def remove_from_group
+    self.group = nil
+    save
+  end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,43 @@
 class Group < ApplicationRecord
+  include ParticularComponent
+
   validates :name, presence: true
 
   has_many :details, dependent: :destroy
+
+  def add_item(item, parent_id = nil)
+    if item.class == Detail
+      item.add_item(self)
+    elsif item.class == Group
+      return if parent_id.blank?
+
+      update(parent_group_id: parent_id)
+    end
+  end
+
+  def remove_item(item)
+    if item.class == Detail
+      item.remove_item
+    elsif item.class == Group
+      if is_most_parent?
+        item.parent_group_id = nil
+        item.save
+
+        return
+      end
+
+      item.parent_group_id = parent_group_id
+      item.save
+    end
+  end
+
+  def get_child
+    children = {}
+    children[:details] = details
+    children
+  end
+
+  def is_most_parent?
+    parent_group_id == nil
+  end
 end
